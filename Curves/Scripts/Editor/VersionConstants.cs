a7c5be34f51bd0e8c1e160a07271e6682d511c3b
Jeff Campbell <jeff@zapdot.com>
2020-03-09 10:48:01 +0100

    v1.1.2
    * Created about window with support, donate info
    * Added menu items for helpful links for reporting bugs, support

---
+++ Curves/Scripts/Editor/VersionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace JCMG.Curves.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		// Version
+		public const string VERSION = "1.1.1";
+	}
+}
